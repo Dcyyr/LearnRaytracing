99e389cd1c765293cc044863870d2dce35a2c8b2
unknown <l17332426256@126.com>
2025-09-01 04:00:28 +0800

    Gamma矫正

--- LearnRayTracing/src/color.h
+++ LearnRayTracing/src/color.h
@@ -3,11 +3,24 @@
 //#include <glm/glm.hpp>
 #include "rtweekend.h"
 
+inline double LinearToGamma(double linearComponent)
+{
+	if(linearComponent >0)
+		return std::sqrt(linearComponent);
+
+	return 0;
+}
+
 void WirteColor(std::ostream& out, const RT::vec3& pixelColor)
 {
 	auto r = pixelColor.x();
 	auto g = pixelColor.y();
 	auto b = pixelColor.z();
+
+	r = LinearToGamma(r);
+	g = LinearToGamma(g);
+	b = LinearToGamma(b);
+
 
 
 	static const Interval intensity(0.000, 0.999);
