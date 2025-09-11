470ef4c71d77a221f09a0f36cb80470e88559689
Linus Li <l17332426256@126.com>
2025-09-11 22:31:35 +0800

    更新封面，采样为500

--- LearnRayTracing/src/main.cpp
+++ LearnRayTracing/src/main.cpp
@@ -12,34 +12,66 @@ int main() {
 	// World
 	HittableList world;
 
+	auto groundMaterial = std::make_shared<Lambertian>(RT::vec3(0.5, 0.5, 0.5));
+	world.Add(std::make_shared<Sphere>(RT::vec3(0, -1000, 0), 1000, groundMaterial));
 
-	auto materialGround = std::make_shared<Lambertian>(RT::vec3(0.8, 0.8, 0.0));
-	auto materialCenter = std::make_shared<Lambertian>(RT::vec3(0.1, 0.2, 0.5));
-	auto materialbubble = std::make_shared<Dielectric>(1.00 / 1.50);
-	auto materialLeft   = std::make_shared<Dielectric>(1.50);
-	auto materialRight  = std::make_shared<Metal>(RT::vec3(0.8, 0.6, 0.2),1.0);
+	for (int a = -11; a < 11; a++)
+	{
+		for (int b = -11; b < 11; b++)
+		{
+			auto chooseMaterial = RandomDouble();
+			RT::vec3 center(a + 0.9 * RandomDouble(), 0.2, b + 0.9 * RandomDouble());
 
+			if ((center - RT::vec3(4, 0.2, 0)).length() > 0.9)
+			{
+				std::shared_ptr<Material> sphereMaterial;
 
+				if (chooseMaterial < 0.8)
+				{
+					auto albedo = RT::vec3::Random() * RT::vec3::Random();
+					sphereMaterial = std::make_shared<Lambertian>(albedo);
+					world.Add(std::make_shared<Sphere>(center, 0.2, sphereMaterial));
+				}
+				else if (chooseMaterial < 0.95) {
 
-    world.Add(std::make_shared<Sphere>(RT::vec3(0, -100.5, -1),100, materialGround));
-	world.Add(std::make_shared<Sphere>(RT::vec3(0, 0, -1.2), 0.5, materialCenter));
-	world.Add(std::make_shared<Sphere>(RT::vec3(-1.0, 0.0, -1.0), 0.5, materialLeft));
-	world.Add(std::make_shared<Sphere>(RT::vec3(-1.0, 0.0, -1.0), 0.4, materialbubble));
-	world.Add(std::make_shared<Sphere>(RT::vec3(1.0, 0.0, -1.0), 0.5, materialRight));
+					auto albedo = RT::vec3::Random(0.5, 1);
+					auto fuzz = RandomDouble(0, 0.5);
+					sphereMaterial = std::make_shared<Metal>(albedo, fuzz);
+					world.Add(std::make_shared<Sphere>(center, 0.2, sphereMaterial));
+
+				}
+				else {
+					sphereMaterial = std::make_shared<Dielectric>(1.5);
+					world.Add(std::make_shared<Sphere>(center, 0.2, sphereMaterial));
+				}
+
+			}
+		}
+	}
+
+	auto material1 = std::make_shared<Dielectric>(1.5);
+	world.Add(std::make_shared<Sphere>(RT::vec3(0, 1, 0), 1.0, material1));
+
+	auto material2 = std::make_shared<Lambertian>(RT::vec3(0.4, 0.2, 0.1));
+	world.Add(std::make_shared<Sphere>(RT::vec3(-4, 1, 0), 1.0, material2));
+
+	auto material3 = std::make_shared<Metal>(RT::vec3(0.7, 0.6, 0.5), 0.0);
+	world.Add(std::make_shared<Sphere>(RT::vec3(4, 1, 0), 1.0, material3));
+	
 
 	Camera m_Cam;
 	m_Cam.m_AspectRatio = 16.0/9.0;
-	m_Cam.m_ImageWidth = 500;
-	m_Cam.m_SamplesPerPixel = 100;
+	m_Cam.m_ImageWidth = 1200;
+	m_Cam.m_SamplesPerPixel = 500;
 	m_Cam.m_MaxDepth = 50;
 
 	m_Cam.m_Vfov = 20.0;
-	m_Cam.m_LookFrom = RT::vec3(-2,2,1);
-	m_Cam.m_LookAt	 = RT::vec3(0, 0, -1);
+	m_Cam.m_LookFrom = RT::vec3(13,2, 3);
+	m_Cam.m_LookAt	 = RT::vec3(0, 0, 0);
 	m_Cam.m_ViewUp	 = RT::vec3(0, 1, 0);
 
-	m_Cam.m_DefocusAngle = 5.0;
-	m_Cam.m_FocusDist    = 3.4;
+	m_Cam.m_DefocusAngle = 0.6;
+	m_Cam.m_FocusDist    = 10.0;
 
 	m_Cam.render(world);
    
